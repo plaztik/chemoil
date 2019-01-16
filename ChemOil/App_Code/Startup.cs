32de251cdfd285d8e8eb0c5c20b521cd4a1d436b
Ewy <tyler.ewy@okstate.edu>
2019-01-16 11:52:45 -0600

    First Git Bash Commit

---
+++ ChemOil/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChemOil.Startup))]
+namespace ChemOil
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ ChemOil/Site.master.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Net.Mail;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SiteMaster : MasterPage
+{
+    private const string AntiXsrfTokenKey = "__AntiXsrfToken";
+    private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
+    private string _antiXsrfTokenValue;
+    public string emailSubject;
+    public string emailMessage;
+    public string email;
+
+    public object WebMail { get; private set; }
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        // The code below helps to protect against XSRF attacks
+        var requestCookie = Request.Cookies[AntiXsrfTokenKey];
+        Guid requestCookieGuidValue;
+        if (requestCookie != null && Guid.TryParse(requestCookie.Value, out requestCookieGuidValue))
+        {
+            // Use the Anti-XSRF token from the cookie
+            _antiXsrfTokenValue = requestCookie.Value;
+            Page.ViewStateUserKey = _antiXsrfTokenValue;
+        }
+        else
+        {
+            // Generate a new Anti-XSRF token and save to the cookie
+            _antiXsrfTokenValue = Guid.NewGuid().ToString("N");
+            Page.ViewStateUserKey = _antiXsrfTokenValue;
+
+            var responseCookie = new HttpCookie(AntiXsrfTokenKey)
+            {
+                HttpOnly = true,
+                Value = _antiXsrfTokenValue
+            };
+            if (FormsAuthentication.RequireSSL && Request.IsSecureConnection)
+            {
+                responseCookie.Secure = true;
+            }
+            Response.Cookies.Set(responseCookie);
+        }
+
+        Page.PreLoad += master_Page_PreLoad;
+    }
+
+    protected void master_Page_PreLoad(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            // Set Anti-XSRF token
+            ViewState[AntiXsrfTokenKey] = Page.ViewStateUserKey;
+            ViewState[AntiXsrfUserNameKey] = Context.User.Identity.Name ?? String.Empty;
+        }
+        else
+        {
+            // Validate the Anti-XSRF token
+            if ((string)ViewState[AntiXsrfTokenKey] != _antiXsrfTokenValue
+                || (string)ViewState[AntiXsrfUserNameKey] != (Context.User.Identity.Name ?? String.Empty))
+            {
+                throw new InvalidOperationException("Validation of Anti-XSRF token failed.");
+            }
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void sendMessage(object sender, EventArgs e)
+    {
+        //var to connect to input
+        emailMessage = Request["message"];
+        email = Request["email"];
+
+
+        System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+        mail.To.Add("tyler.ewy@gmail.com");
+        mail.From = new MailAddress("from gmail address", "Email head", System.Text.Encoding.UTF8);
+        mail.Subject = email;
+        mail.SubjectEncoding = System.Text.Encoding.UTF8;
+        mail.Body = emailMessage;
+        mail.BodyEncoding = System.Text.Encoding.UTF8;
+        mail.IsBodyHtml = true;
+        mail.Priority = MailPriority.High;
+        SmtpClient client = new SmtpClient();
+        client.Credentials = new System.Net.NetworkCredential("tyler.ewy@gmail.com", "Schecter88");
+        client.Port = 587;
+        client.Host = "smtp.gmail.com";
+        client.EnableSsl = true;
+        try
+        {
+            client.Send(mail);
+            Page.RegisterStartupScript("UserMsg", "<script>alert('Successfully Send...');if(alert){ window.location='SendMail.aspx';}</script>");
+        }
+        catch (Exception ex)
+        {
+            Exception ex2 = ex;
+            string errorMessage = string.Empty;
+            while (ex2 != null)
+            {
+                errorMessage += ex2.ToString();
+                ex2 = ex2.InnerException;
+            }
+            Page.RegisterStartupScript("UserMsg", "<script>alert('Sending Failed...');if(alert){ window.location='SendMail.aspx';}</script>");
+        }
+
+    }
+
+    protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
+    {
+        Context.GetOwinContext().Authentication.SignOut();
+    }
+}
